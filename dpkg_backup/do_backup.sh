c6709e2f9fc2099404731c228d87c7a3f35ae8a4
Shriniwas Sharma <shriniwas26@gmail.com>
2018-01-16 23:24:03 +0100

    Minor change

--- dpkg_backup/do_backup.sh
+++ dpkg_backup/do_backup.sh
@@ -1,4 +1,4 @@
 dpkg --get-selections > ./Package.list &&
-sudo cp -R /etc/apt/sources.list* ./ &&
+sudo cp -Rv /etc/apt/sources.list* ./ &&
 sudo apt-key exportall > ./Repo.keys &&
 echo "Backup done!"
