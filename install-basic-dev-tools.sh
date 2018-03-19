93ca7ec8587eb01aa43e56881583cdd048fec8a8
Shriniwas Sharma <shriniwas26@gmail.com>
2018-03-19 14:07:45 +0100

    Vimrc & bash-it

--- install-basic-dev-tools.sh
+++ install-basic-dev-tools.sh
@@ -5,7 +5,15 @@ apt install -y vim git-cola tmux
 cp ./.tmux.conf ~/
 
 # Install atom
-wget https://atom.io/download/deb -O /tmp/atom.deb 
+wget https://atom.io/download/deb -O /tmp/atom.deb
 dpkg -i /tmp/atom.deb || apt install -f
 
+# Install bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it &&
+~/.bash_it/install.sh &&
+echo "Installed Bash-it"
 
+# Install vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime &&
+sh ~/.vim_runtime/install_awesome_vimrc.sh &&
+echo "Installed Awesome vimrc"
