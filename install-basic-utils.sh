175f32c644c049045d996b567643f5dfefae61a4
Shriniwas Sharma <shriniwas26@gmail.com>
2018-05-31 13:28:40 +0200

    Minor update

--- install-basic-utils.sh
+++ install-basic-utils.sh
@@ -8,12 +8,20 @@ echo "downloading google chrome latest stable edition" &&
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb &&
 dpkg -i /tmp/google-chrome-stable_current_amd64.deb &&
 rm -f /tmp/google-chrome-stable_current_amd64.deb
-) || sudo apt install -f
+) || (sudo apt install -f && echo "Google Chrome Installed")
 
-# Install RedShift
+# Install RedShift & Chromium Browser
 
-sudo apt install -y redshift redshift-gtk
+sudo apt install -y redshift redshift-gtk chromium-browser
 
-# Install Chromium
-
-sudo apt install -y chromium-browser
+# Install Mendeley
+(
+echo "Installing Mendeley..." &&
+rm -f mendeleydesktop-latest.deb &&
+wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest -O mendeleydesktop-latest.deb &&
+dpkg -i mendeleydesktop-latest.deb &&
+) ||
+(
+sudo apt install -f &&
+echo "Mendeley Installed"
+)
