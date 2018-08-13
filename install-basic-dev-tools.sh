0ee97cf8baa040b494c386ccb5a4a0943bb5cbc1
Shriniwas Sharma <shriniwas26@gmail.com>
2018-08-13 16:11:12 +0200

    Updated install script for dev tools

--- install-basic-dev-tools.sh
+++ install-basic-dev-tools.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-apt install -y vim git-cola tmux
+sudo apt install -y vim git-cola tmux
 
 # Copy tmux conf
-cp ./.tmux.conf ~/
+TMUX_CONF="$HOME/.tmux.conf"
+if [ ! -f $TMUX_CONF  ];then
+    echo "Copying .tmux.conf"
+    cp ./.tmux.conf $TMUX_CONF
+else
+    echo "Tmux config file already exists, not copying"
+fi
 
 # Install atom
-wget https://atom.io/download/deb -O /tmp/atom.deb
-dpkg -i /tmp/atom.deb || apt install -f
+ATOM_DEB="/tmp/atom.deb"
+if [ ! -f $ATOM_DEB  ];then
+    echo "Downloading Atom installer"
+    wget https://atom.io/download/deb -O $ATOM_DEB
+else
+    echo "Atom installer already exists at $ATOM_DEB"
+fi
+
+sudo dpkg -i $ATOM_DEB || sudo apt install -f
 
 # Install bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it &&
