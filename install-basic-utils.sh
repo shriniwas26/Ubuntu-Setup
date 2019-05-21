8320afa6a28335673dad7ba4df92b2056fd73809
Shriniwas Sharma <shriniwas26@users.noreply.github.com>
2019-05-21 14:31:53 +0200

    Update install-basic-utils.sh

--- install-basic-utils.sh
+++ install-basic-utils.sh
@@ -6,7 +6,7 @@
 rm -f /tmp/google-chrome-stable_current_amd64.deb &&
 echo "downloading google chrome latest stable edition" &&
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb &&
-dpkg -i /tmp/google-chrome-stable_current_amd64.deb &&
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb &&
 rm -f /tmp/google-chrome-stable_current_amd64.deb
 ) || (sudo apt install -f && echo "Google Chrome Installed")
 
@@ -19,7 +19,7 @@ sudo apt install -y redshift redshift-gtk chromium-browser
 echo "Installing Mendeley..." &&
 rm -f mendeleydesktop-latest.deb &&
 wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest -O mendeleydesktop-latest.deb &&
-dpkg -i mendeleydesktop-latest.deb
+sudo dpkg -i mendeleydesktop-latest.deb
 ) ||
 (
 sudo apt install -f &&
